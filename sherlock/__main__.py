6efb1a5dc89c807f54aac8c28440daa9a74776d6
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2021-05-12 18:03:23 +1000

    Use relative imports

--- sherlock/__main__.py
+++ sherlock/__main__.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         print("Sherlock requires Python 3.6+\nYou are using Python %s, which is not supported by Sherlock" % (python_version))
         sys.exit(1)
 
-    import sherlock
+    from . import sherlock
     sherlock.main()
